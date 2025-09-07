38340cb20585a8afbed349711a9baa30b6af90ec
Дмитрий Майоров <dimayorov@yandex.ru>
2025-09-07 08:42:00 +0300

    Add tests

--- news/pytest_trial/test_example.py
+++ news/pytest_trial/test_example.py
@@ -1,4 +1,8 @@
 # test_example.py
+import pytest
+from time import sleep
+
+
 def one_more(x):
     return x + 1
 
@@ -8,24 +12,54 @@ def get_sort_list(str):
     return new_list
 
 
-def test_correct():
-    print('Правильный тест')  # Новая строка.
-    assert one_more(4) == 5
-
-
-def test_fail():
-    print('Неправильный тест')  # Новая строка.
-    assert one_more(3) == 5
+@pytest.mark.parametrize(
+    'input_arg, expected_result',
+    [
+        (4, 5), 
+        pytest.param(3, 5, marks=pytest.mark.xfail)  # Ожидается падение теста.
+    ],
+    ids=['First parameter', 'Second parameter',]
+)
+def test_one_more(input_arg, expected_result):
+    assert one_more(input_arg) == expected_result
 
 
 def test_sort():
-    """Тестируем функцию get_sort_list()."""
+    """Тестируем функцию get_sort_list()."""    
     result = get_sort_list('Яша, Саша, Маша, Даша')
     assert result == ['Даша', 'Маша', 'Саша', 'Яша']
 
+@pytest.mark.slow  # Отмечаем маркером тест.
 def test_type():
     """Тестируем тип данных, возвращаемых из get_sort_list()."""
+    sleep(3)  # Трёхсекундная пауза.
     result = get_sort_list('Яша, Саша, Маша, Даша')
-    # Провальный тест:
-    # ожидаем число, но вернётся список.
     assert isinstance(result, int)
+
+@pytest.mark.xfail(reason='Пусть пока падает, завтра починю.')
+def test_false():
+    assert False
+
+def cartesian_product(a, b):
+    return a * b
+
+
+@pytest.mark.parametrize('x', [1, 2])
+@pytest.mark.parametrize('y', ['one', 'two'])
+def test_cartesian_product(x, y):
+    assert cartesian_product(x, y) is not None 
+
+@pytest.fixture
+def give_me_a_string():
+    return 'Какой чудесный день!'
+
+
+# Новая фикстура возвращает список со строкой из первой фикстуры.
+@pytest.fixture
+def pack_to_list(give_me_a_string):  # Фикстура может вызывать другие фикстуры.
+    return [give_me_a_string]
+
+
+# Тестовая функция использует обе фикстуры и проверяет их содержимое.
+def test_string_fixture(pack_to_list, give_me_a_string):  
+    assert pack_to_list == [give_me_a_string] 
